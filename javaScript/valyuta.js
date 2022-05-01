1a7931d1d78635abdf0f59e3d52bae9e0c4bd277
rashadmemmedov <talehg2932@gmail.com>
2022-05-01 07:01:43 +0400

    layihenin responsivliyinin yazilmasi

--- javaScript/valyuta.js
+++ javaScript/valyuta.js
@@ -1,10 +1,20 @@
+//#region buttonlara uygun default deyerlerin Api dan getirilmesi
 let rightBtnId = 'USD';
 let leftBtnId = 'RUB';
+//#endregion
+
+let getAddInputValue;
 
 let tableLeftBtn = document.querySelectorAll('.aviable-btn');
 var convertLeft = document.querySelector('.convert-left');
 var convertRight = document.querySelector('.convert-right');
-var textBtn = tableLeftBtn[0].textContent;
+
+
+// get inputs value
+let getLeftInputValue=document.getElementById('left-input');
+let getRightInputValue=document.getElementById('right-input');
+// end
+
 for (let i = 0; i < tableLeftBtn.length; i++) {
     tableLeftBtn[i].addEventListener('click', () => {
         tableLeftBtn.forEach(element => {
@@ -15,6 +25,7 @@ for (let i = 0; i < tableLeftBtn.length; i++) {
         getData(`https://api.exchangerate.host/latest?base=${rightBtnId}&symbols=${leftBtnId}`)
     })
 }
+
 let tableRightBtn = document.querySelectorAll('.sell-btn');
 for (let j = 0; j < tableRightBtn.length; j++) {
     tableRightBtn[j].addEventListener('click', () => {
@@ -26,19 +37,77 @@ for (let j = 0; j < tableRightBtn.length; j++) {
         getData(`https://api.exchangerate.host/latest?base=${rightBtnId}&symbols=${leftBtnId}`);
     })
 }
-//  Api hissenin yazilmasi
+
+
+
+//#region Api-dan datanin cagrilmasi
 async function getData(url) {
     let api = await fetch(url);
     let jsonData = await api.json();
     var values = Object.values(jsonData.rates);
     convertRight.innerText = `1 ${rightBtnId} = ${(values[0]).toFixed(4)} ${leftBtnId}`;
+    
+    //#region  input valuesinin sifirlanmasi button deyisdikde ona uygun valu-larin
+    if(getRightInputValue.value===''){
+        getLeftInputValue.value=" ";
+    }
+    else{
+        if(rightBtnId==leftBtnId){
+            getRightInputValue.value=getLeftInputValue.value;
+        }else{
+            getLeftInputValue.value=(Number(getRightInputValue.value)*values[0]).toFixed(4);
+            getRightInputValue=getRightInputValue;
+        }
+    }
+   //#endregion
+
+   //#region  inputa deyer daxil edildikde valuenin hesablanmasi
+    getRightInputValue.addEventListener('input',()=>{
+        if(getRightInputValue.value===''){
+            getLeftInputValue.value=" ";
+        }
+        else{
+            getLeftInputValue.value=(Number(getRightInputValue.value)*values[0]).toFixed(4);
+        }
+       
+    })
+    //#endregion
+
+
     async function getDataConvert(url) {
         let api = await fetch(url);
         let jsonData = await api.json();
         var values = Object.values(jsonData.rates);
+        
         convertLeft.innerText = `1 ${leftBtnId} = ${((values[0])).toFixed(4)} ${rightBtnId}`;
+        //#region  input valuesinin sifirlanmasi button deyisdikde ona uygun valu-larin
+        if(getLeftInputValue.value===''){
+            getRightInputValue.value=" ";
+        }
+        else{
+            if(leftBtnId===rightBtnId){
+                getLeftInputValue.value=getRightInputValue.value;
+            }else{
+                 getRightInputValue.value=(Number(getLeftInputValue.value)*values[0]).toFixed(4);
+            }
+        }
+       //#endregion
+
+       //#region  inputa deyer daxil edildikde valuenin hesablanmasi
+        getLeftInputValue.addEventListener('input',()=>{
+            if(getLeftInputValue.value===''){
+                getRightInputValue.value=" ";
+            }
+            else{
+                getLeftInputValue=getLeftInputValue;
+                getRightInputValue.value=(Number(getLeftInputValue.value)*values[0]).toFixed(4);
+            }   
+        })
+        //#endregion
     }
     getDataConvert(`https://api.exchangerate.host/latest?base=${leftBtnId}&symbols=${rightBtnId}`);
 }
 getData(`https://api.exchangerate.host/latest?base=${rightBtnId}&symbols=${leftBtnId}`);
-// Api hissenin sonu
+
+
+
